c1541e980b7ba1f8ce3718ec8abf0c555ed81e6c
Big Trees <traveye@protonmail.com>
2023-01-30 22:59:18 -0800

    adding heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { writeToFile, readAndAppend, readFile } = require('./helpers/fsUtil');
 const shortid = require('shortid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
